7a7722d850b6897f7bbfeb4a621f478ade95066d
Jusk <speranskiy.job@gmail.com>
2023-02-16 13:38:57 +0400

    create kbs

---
+++ src/bot/keyboards/__init__.py
@@ -0,0 +1,9 @@
+from .menu import generate_general_kb, generate_random_kb
+from .pagination import generate_photo_pagination, PaginationKeyboard
+
+__all__ = (
+    'generate_general_kb',
+    'generate_random_kb',
+    'generate_photo_pagination',
+    'PaginationKeyboard',
+)
